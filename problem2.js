2f470a8d82ec8679da83b4c2dc9aec04c4c6c002
lsh <ls940131@naver.com>
2024-11-20 22:31:58 +0900

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -1,18 +1,19 @@
 const input = "022";
+// const input = "23";
 
 function solution(S) {
-	const result = [];
+	const results = new Set();
 
 	for (let i = 0, length = S.length; i < length; i++) {
 		for (let digit = 0; digit <= 9; digit++) {
 			const cur = S.slice(0, i) + digit + S.slice(i + 1);
 
-			if (!result.includes(cur) && parseInt(cur) % 3 == 0) {
-				result.push(cur);
+			if (parseInt(cur) % 3 == 0) {
+				results.add(cur);
 			}
 		}
 	}
 
-	return result.length;
+	return results.size;
 }
 console.log(solution(input));
