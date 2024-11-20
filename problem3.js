69469974ffed93c8d5255c7c93bee80213a76197
lsh <ls940131@naver.com>
2024-11-20 21:54:35 +0900

    Update problem3.js
    map -> object

--- problem3.js
+++ problem3.js
@@ -3,22 +3,22 @@ const input = [1, 5, 2, 4, 3, 3];
 // const input = [9, 9, 9, 9, 9];
 
 function solution(A) {
-	const sums = new Map();
+	const sumMap = {};
 
 	for (let i = 0, length = A.length - 1; i < length; i++) {
 		const sum = A[i] + A[i + 1];
-		if (!sums.has(sum)) {
-			sums.set(sum, []);
+		if (!sumMap[sum]) {
+			sumMap[sum] = [];
 		}
-		sums.get(sum).push(i);
+		sumMap[sum].push(i);
 	}
 
 	let result = 0;
-	for (const [key, value] of sums) {
+	for (const key of Object.keys(sumMap)) {
 		let count = 0;
 		let lastEnd = -1;
 
-		for (const index of value) {
+		for (const index of sumMap[key]) {
 			if (index > lastEnd) {
 				count++;
 				lastEnd = index + 1;
